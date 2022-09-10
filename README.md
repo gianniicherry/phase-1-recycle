8c1dd9be9c632e0a988f80af7205633efbce5dad
Gianni <gianniicherry@gmail.com>
2022-09-10 08:02:53 -0700

    my changes

---
+++ README.md
@@ -0,0 +1,14 @@
+# Terraforge
+Terraforge is a platform designed for users to easily recycle, bid, and get paid out on Electronic waste. E-waste is a massive issue, with over 50 million tons of electronic waste generated annually. Terraforge creates a one-stop shop for users to recycle or sell their waste, and receive payouts for doing so.
+
+
+As a user, you will be able to submit an item they would like to recycle or list for sale. Once the item is submitted, it will be listed on our database.
+
+With future functionality, we plan to integrate an auction style system designed to create a strong and high demand app-based economy. Each item submitted will have a quantitative value backed on an ERC20 token, which you can redeem at any item once you have submitted an item to be recycled. If you opt to list it for sale, any user will be able to bid on items listed for sale in cash, and receive the token value in instead, at which point the user submitted will receive the cash value instead of tokens. This ensures the end user will still receive cash value or token value based bids received, and users without electronics can still invest and receive tokens through our auction style system.
+
+Per financial quarter, Terraforge will allocate a set percentage of revenue generated from recycling users e-waste to burn the circulating supply of tokens, limiting volume and increasing value of each individual token.
+
+### Installation Instructions
+- Fork and clone
+- open index.html in browser
+- enjoy! 
--- javascripts/index.js
+++ javascripts/index.js
@@ -1,14 +1,58 @@
 /** Global Variables **/
+const baseUrl = 'http://localhost:3000'
+let items = [];
 
 /** Node Getters */
 const mainDiv = () => document.getElementById('main');
-const HomeLink = () => document.getElementById('home-link')
+const homeLink = () => document.getElementById('home-link');
+const recycleLink = () => document.getElementById('recycle');
+const saleLink = () => document.getElementById('for-sale');
+const itemDescription = () => document.getElementById('item-description');
+const recycleOption = () => document.getElementById('item-option');
+const brandOption = () => document.getElementById('brand-option');
+const workingOption = () => document.getElementById('item-working');
+const weight = () => document.getElementById('weight');
+
+
 /** Event Listeners */
 const attachHomePageLinkEvent = () => {
-    HomeLink().addEventListener('click', loadHome)
+    homeLink().addEventListener('click', loadHome)
+}
+const attachRecycleLinkEvent = () => {
+    recycleLink().addEventListener('click', loadRecycle)
+}
+const attachSaleLinkEvent = () => {
+    saleLink().addEventListener('click', loadSale)
 }
 
 /** Event Handlers */
+
+ const submitForm = event => {
+    event.preventDefault();
+  
+    const jsonObject = {
+        description: itemDescription().value,
+        item: recycleOption().value,
+        brand: brandOption().value,
+        working: workingOption().value,
+        weight: weight().value,
+    };
+    fetch(baseUrl + '/items', {
+        method: "POST",
+        headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+        },
+        body: JSON.stringify(jsonObject)
+    })
+        .then(response => response.json())
+        .then(data => {
+          items.push(data)
+          loadSale()
+        })
+}
+
+
 const loadHome = () => {
     resetMainDiv();
     const h1 = document.createElement("h1")
@@ -18,9 +62,155 @@ const loadHome = () => {
     p.className = 'center-align'
 
     h1.innerText = 'Welcome to Terraforge'
-    p.innerText = 'Terraforge is helping people find ways to recycle their old electronics and connect users with like minded people along the way.'
+    p.innerText = 'Welcome to Terraforge! We are a platform designed for users to easily recycle, bid, and get paid out on Electronic waste. E-waste is a massive issue, with over 50 million tons of electronic waste generated annually. Terraforge creates a one-stop shop for users to recycle or sell their waste, and receive payouts for doing so.'
     mainDiv().appendChild(h1);
     mainDiv().appendChild(p);
+}
+
+const loadRecycle = event => {
+    event.preventDefault();
+    resetMainDiv();
+    const h1 = createH1('Recycle')
+    const form = document.createElement('form')
+    const row1 = createRow();
+    const row2 = createRow();
+    const row3 = createRow();
+
+    const div1 = createTextField('item-description','Describe the item you want to recycle', 's6');
+    const div2 = createSelectField('item-option','s6','I want to recycle...','Item',["Laptop", "Smartphone", "Tablet"]);
+    const div3 = createSelectField('brand-option','s6','Choose','Brand',["Apple", "Samsung", "Other"]);
+    const div4 = createSelectField('item-working','s6','Choose','Does this item work?',["Yes", "No"]);
+    const div5 = createTextField('weight','Approx. weight', 's6');
+
+    const submit = document.createElement('input');
+    submit.setAttribute('type','submit');
+    submit.setAttribute('id', 'submit-form');
+    submit.className = 'btn black';
+
+    row1.appendChild(div1);
+
+    row2.appendChild(div2);
+    row2.appendChild(div3);
+
+    row3.appendChild(div4);
+    row3.appendChild(div5);
+
+    form.appendChild(row1);
+    form.appendChild(row2);
+    form.appendChild(row3);
+    form.appendChild(submit);
+    
+
+    mainDiv().appendChild(h1);
+    mainDiv().appendChild(form);
+
+    form.addEventListener('submit', submitForm);
+
+    $(document).ready(function(){
+        $('select').formSelect();
+    });
+}
+
+const loadSale = event => {
+    if(event) {
+        event.preventDefault();
+    }
+    resetMainDiv()
+    const h1 = document.createElement('h1')
+    const div = document.createElement('div')
+    
+    h1.textContent = 'For Sale';
+
+
+    div.className = 'collection';
+
+    items.forEach(itemObj => {
+        const a = document.createElement('a');
+        a.className = 'collection-item';
+        a.innerText = itemObj.description
+
+        div.appendChild(a);
+    })
+
+
+    mainDiv().appendChild(h1);
+    mainDiv().appendChild(div);
+}
+/** REQUESTS */
+
+const loadItems = () => {
+    fetch(baseUrl + '/items')
+        .then(resp => resp.json())
+        .then(data => {
+            console.log(data, "data")
+            items = data;
+        })
+}
+
+/** NODE Creators */
+
+const createRow = () => {
+    const div = document.createElement('div')
+    div.className = "row"
+    return div;
+}
+
+const createH1 = text => {
+    const h1 = document.createElement('h1')
+    h1.innerText = text;
+    return h1;
+}
+
+const createFormCol = colSize => {
+    const div = document.createElement('div')
+    div.className = 'input-field col ' + colSize;
+    return div
+}
+
+const createTextField = (id, labelText, colSize) => {
+    const div = createFormCol(colSize)
+    const label = document.createElement('label')
+    const input = document.createElement('input')
+
+    input.setAttribute('type', 'text');
+    input.setAttribute('id' , id);
+
+    label.setAttribute('for', id);
+    label.innerText = labelText;
+
+    div.appendChild(input);
+    div.appendChild(label);
+
+    return div;
+}
+
+const createSelectField = (id, colSize, placeholder, labelText, options=[]) => {
+    const div = createFormCol(colSize)
+    const select = document.createElement('select');
+    const option = document.createElement('option');
+    const label = document.createElement('label');
+
+    select.setAttribute('id', id);
+    option.setAttribute('disabled', true);
+    option.setAttribute('selected', true);
+    option.value = '';
+    option.innerText = placeholder
+    label.setAttribute('id', id)
+    label.innerText = labelText;
+
+    select.appendChild(option);
+
+    options.forEach( optionText => {
+        const option = document.createElement('option')
+        option.innerText = optionText;
+        option.value = optionText;
+        select.appendChild(option);
+    })
+
+    div.appendChild(select);
+    div.appendChild(label);
+
+    return div;
 }
 /** MISC */
 const resetMainDiv = () => {
@@ -32,5 +222,9 @@ const resetMainDiv = () => {
 document.addEventListener('DOMContentLoaded', () => {
 
 })
+loadItems();
 loadHome();
 attachHomePageLinkEvent();
+attachRecycleLinkEvent();
+attachSaleLinkEvent();
+
